6acd888aef1c99888321d2201afe07ac1d7bebbc
Sergio Jiménez <49730454+sergio-jr-dev@users.noreply.github.com>
2026-09-14 13:22:10 +0200

    refactor: remove temporary quiz context adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Footer } from './components/footer/Footer';
 import { Header } from './components/header/Header';
 import { Game } from './components/quiz/game/Game';
-import { QuizContextProvider } from './context/QuizContextProvider.tsx';
 
 const App = () => {
   return (
     <>
       <Header />
-      <QuizContextProvider>
-        <Game />
-      </QuizContextProvider>
+      <Game />
       <Footer />
     </>
   );
